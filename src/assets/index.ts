d0524b444393937f5f1902a10a55359396c75c21
EnzoWu <75583251+EnzoWu479@users.noreply.github.com>
2023-02-16 17:08:41 -0300

    fix: img

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+export { default as bg_day } from "./background/bgday.jpg";
+export { default as bg_night } from "./background/bgnight.png";
+
+export { default as game_kanto } from "./games/kanto.jpg";
+export { default as game_johto } from "./games/johto.jpg";
+export { default as game_hoenn } from "./games/hoenn.jpg";
+export { default as game_sinnoh } from "./games/sinnoh.png";
+export { default as game_unova } from "./games/unova.jpg";
